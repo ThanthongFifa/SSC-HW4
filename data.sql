0e2f779856f9bb734f09059d76f3cb6906adb6ad
ThanthongFifa <Fifa0962@gmail.com>
2022-03-16 00:13:47 +0700

    Now able to run on server

---
+++ data.sql
@@ -0,0 +1,43 @@
+-- MySQL dump 10.13  Distrib 8.0.28, for macos12.2 (x86_64)
+--
+-- Host: 127.0.0.1    Database: login_webapp
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.7.3-MariaDB-1:10.7.3+maria~focal
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tbl_user`
+--
+
+DROP TABLE IF EXISTS `tbl_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tbl_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `display_name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `tbl_user_username_uindex` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_user`
+--
+
+LOCK TABLES `tbl_user` WRITE;
+/*!40000 ALTER TABLE `tbl_user` DISABLE KEYS */;
+INSERT INTO `tbl_user` VALUES (7,'admin','$2a$10$EL8/orTF8.U2AhlIRF58S.LtWPc7cyhNpaeT.Z4bUQw3q.gsjksM2','fifa');
+/*!40000 ALTER TABLE `tbl_user` ENABLE KEYS */;
+UNLOCK TABLES;
---
+++ startSQL.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker run -p 127.0.0.1:3306:3306 -p 172.17.0.1:3306:3306 --name mariadb -e MARIADB_ROOT_PASSWORD=password -d --restart=always mariadb:10
